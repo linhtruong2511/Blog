4489ce9a7ba7e69952ca052952f00ba6cf040838
linhtruong <linhtk2511044@gmail.com>
2025-05-25 08:28:25 +0700

    Fix  (Button loading): Upload multi post

--- src/pages/admin/uploadPost/UploadPost.tsx
+++ src/pages/admin/uploadPost/UploadPost.tsx
@@ -11,7 +11,8 @@ import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { CloudUpload, Undo2 } from "lucide-react";
+import { CloudUpload, Loader, Loader2, Undo2 } from "lucide-react";
+import { toast } from "react-toastify";
 
 interface Props {
   content: string;
@@ -24,6 +25,7 @@ export default function UploadPost({ content, onBackToEdit }: Props) {
   const preview = useRef<HTMLDivElement | null>(null);
   const db = useDB();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (preview.current) {
@@ -51,11 +53,14 @@ export default function UploadPost({ content, onBackToEdit }: Props) {
       lastUpdate: new Date().toLocaleDateString("vi-VN"),
     };
     try {
+      setIsLoading(true);
       await addDoc(collection(db, "post"), {
         ...data,
       });
       setTimeout(() => {
+        setIsLoading(false);
         navigate("/admin");
+        toast.success('Tạo bài viết thành công !!!');
       }, 1000);
     } catch (e) {
       console.log(e);
@@ -65,21 +70,20 @@ export default function UploadPost({ content, onBackToEdit }: Props) {
   const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
     title.current = e.target.value;
   };
+
   const handleChangeDecs = (e: ChangeEvent<HTMLTextAreaElement>) => {
     shortDecs.current = e.target.value;
   };
+
   return (
     <>
-      <Button variant={'secondary'} onClick={onBackToEdit} className="mb-4">
+      <Button variant={"secondary"} onClick={onBackToEdit} className="mb-4">
         Quay lại <Undo2 />
-      </Button> 
+      </Button>
       <div className="flex flex-col gap-4">
         <div className="grid w-full gap-1.5">
           <Label htmlFor="title">Tiêu đề</Label>
-          <Input
-            id="title"
-            onChange={handleChangeTitle}
-          />
+          <Input id="title" onChange={handleChangeTitle} />
         </div>
         <div className="grid w-full gap-1.5">
           <Label htmlFor="desc">Mô tả</Label>
@@ -101,9 +105,16 @@ export default function UploadPost({ content, onBackToEdit }: Props) {
           <div className="article-content" ref={preview}></div>
         </div>
         <div className="text-right pt-5">
-          <Button onClick={handlePost}>
-            Đăng bài viết <CloudUpload />
-          </Button>{" "}
+          {!isLoading ? (
+            <Button onClick={handlePost}>
+              Đăng bài viết <CloudUpload />
+            </Button>
+          ) : (
+            <Button onClick={handlePost} disabled>
+              <Loader2 className="animate-spin" />
+              Đang tải bài viết
+            </Button>
+          )}
         </div>
       </div>
     </>
