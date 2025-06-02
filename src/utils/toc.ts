5f73c73759daf62deac9c8203a19050a172f0427
linhtruong <linhtk2511044@gmail.com>
2025-06-02 11:04:10 +0700

    Fix: double TOC

--- src/utils/toc.ts
+++ src/utils/toc.ts
@@ -1,14 +1,15 @@
 export const createToc = (content: HTMLDivElement, toc: HTMLUListElement) => {
+  toc.innerHTML = "";
   const headings = content.querySelectorAll("h1, h2, h3");
   for (let i = 0; i < headings.length; i++) {
     const li = document.createElement("li");
     const id = headings[i].id || "toc-" + i;
     headings[i].id = id;
-    li.innerHTML = '';
+    li.innerHTML = "";
     li.innerHTML = `
       <a href="#${id}" >${headings[i].textContent}</a>
     `;
-    li.className = ''
+    li.className = "";
     li.style.setProperty(
       "margin-left",
       (Number.parseInt(headings.item(i).tagName.at(1) as string) - 1) * 30 +
