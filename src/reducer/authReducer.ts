68c70610d1c800420869c97ab0c952c26251504b
linhtruong <linhtk2511044@gmail.com>
2025-06-02 23:12:15 +0700

    Update: User posts reducer

--- src/reducer/authReducer.ts
+++ src/reducer/authReducer.ts
@@ -1,4 +1,5 @@
 import { AuthState } from "@/types/AuthState";
+import PostType from "@/types/PostType";
 import { UserType } from "@/types/UserType";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
@@ -6,6 +7,12 @@ const initialState: AuthState = {
   isAuthenticated: false,
   user: null,
   loading: true,
+  posts: [],
+};
+
+type PostUserUpdate = {
+  id: string;
+  newData: object;
 };
 
 const authSlice = createSlice({
@@ -25,16 +32,42 @@ const authSlice = createSlice({
       state.loading = false;
     },
 
-    updateAuthUser: (state, action : PayloadAction<object>) => {
+    updateAuthUser: (state, action: PayloadAction<object>) => {
       const newUser = {
         ...state.user,
         ...action.payload,
-      }
-      
+      };
+
       state.user = newUser as UserType;
-    }
+    },
+
+    setUserPosts: (state, action: PayloadAction<PostType[]>) => {
+      state.posts = action.payload;
+    },
+    updateUserPosts: (state, action: PayloadAction<PostUserUpdate>) => {
+      state.posts = state.posts.map((post) => {
+        if (post.id === action.payload.id) {
+          return {
+            ...post,
+            ...action.payload.newData,
+          };
+        }
+        return post;
+      });
+    },
+    removeUserPosts: (state, action: PayloadAction<string>) => {
+      state.posts = state.posts.filter((post) => post.id !== action.payload);
+    },
   },
 });
 
 export default authSlice.reducer;
-export const { loginSuccess, logout, loadingFinish, updateAuthUser } = authSlice.actions;
+export const {
+  loginSuccess,
+  logout,
+  loadingFinish,
+  updateAuthUser,
+  setUserPosts,
+  updateUserPosts,
+  removeUserPosts,
+} = authSlice.actions;
